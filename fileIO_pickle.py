4dbff181f5c7a48995a6e26af0357461414c1ba5
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-05-03 13:45:02 +0100

    Created fileIO_pickle
    Using pickle to serializing and de-serializing python object structure

---
+++ fileIO_pickle.py
@@ -0,0 +1,20 @@
+##########################################################
+#Using pickle to serializing and de-serializing python object structure 
+import pickle
+class Area:
+	"""This is an information gathering program for the community."""
+	def __init__(self, name, street, level):
+		super(Area, self).__init__()
+		self.name = name
+		self.street = street
+		self.level = level
+	def __repr__(self):
+		return f"{self.name} is from {self.street} street and a {self.level} member of the Dev team."
+jare = Area("Adejare","Osun","Professional")
+
+with open("Jare", "wb") as file:
+	pickle.dump(jare, file)
+with open("Jare", "rb") as file:
+	clean = pickle.load(file)
+	print(clean)
+print(jare.__doc__)
