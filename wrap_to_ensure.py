3dec6b692910e24145dba052cbdcb28a843821d7
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-05-03 13:55:57 +0100

    Formatted with pylance

--- wrap_to_ensure.py
+++ wrap_to_ensure.py
@@ -1,15 +1,19 @@
-#Convert input to ensure it's printed in desired type
+# Convert input to ensure it's printed in desired type
 import functools
+
+
 def ensure(*op):
-	def decorator(fn):
-		def wrapper(*args, **kwargs):
-			args = [i(j) for (i,j) in zip(op,args)]
-			return fn(*args,**kwargs)
-		return wrapper
-	return decorator
+    def decorator(fn):
+        def wrapper(*args, **kwargs):
+            args = [i(j) for (i, j) in zip(op, args)]
+            return fn(*args, **kwargs)
+        return wrapper
+    return decorator
 
-@ensure(int,float)
-def tell_info(quantity,price):
-	return f"The cost of {quantity} is ${price}"
 
-print(tell_info(12,13.49))
+@ensure(int, float)
+def tell_info(quantity, price):
+    return f"The cost of {quantity} is ${price}"
+
+
+print(tell_info(12, 13.49))
