90d92e5050b05f85b9144df78e76c72565eb7845
Idris Akintobi Adejare <akintobiidris@gmail.com>
2022-05-03 13:49:01 +0100

    created wrap to ensure
    Using wrapper to ensure a certain type of data is passed

---
+++ wrap_to_ensure.py
@@ -0,0 +1,15 @@
+#Convert input to ensure it's printed in desired type
+import functools
+def ensure(*op):
+	def decorator(fn):
+		def wrapper(*args, **kwargs):
+			args = [i(j) for (i,j) in zip(op,args)]
+			return fn(*args,**kwargs)
+		return wrapper
+	return decorator
+
+@ensure(int,float)
+def tell_info(quantity,price):
+	return f"The cost of {quantity} is ${price}"
+
+print(tell_info(12,13.49))
